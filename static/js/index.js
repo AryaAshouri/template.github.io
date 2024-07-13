3ba71be240aae44e945a712adecb10795ccf517e
Arya <arya.tesla1856@gmail.com>
2024-07-12 19:16:03 -0700

    changed the layout completely

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,46 @@
 function home(){
   location.reload();
 }
+function skill(){
+  window.scrollTo(0, 850);
+}
+function project(){
+  window.scrollTo(0, 1700);
+}
+function about(){
+  window.scrollTo(0, 2500);
+}
+function resume(){
+  window.scrollTo(0, 3180);
+}
+function contact(){
+  window.scrollTo(0, 3980);
+}
+function first_project(){
+  window.location.href = "https://github.com/AryaAshouri/GuessTheNumber-csharp-winform"
+}
+function second_project(){
+  window.location.href = "https://github.com/AryaAshouri/Manager-discord-bot"
+}
+function third_project(){
+  window.location.href = "https://github.com/AryaAshouri/Webshop-django-webapp"
+}
+function first_resume(){
+  window.location.href = "https://quera.org/profile/Red_Ninja"
+}
+function second_resume(){
+  window.location.href = "https://github.com/AryaAshouri"
+}
+function first_contact(){
+  window.location.href = "mailto:arya.tesla1856@gmail.com"
+}
+function second_contact(){
+  window.location.href = "https://t.me/AryaAshouri"
+}
+function third_contact(){
+  window.location.href = "https://www.discordapp.com/users/1053361308029956148"
+}
+function fourth_contact(){
+  window.location.href = "https://web.bale.ai/@arya_ashouri"
+}
 AOS.init();
